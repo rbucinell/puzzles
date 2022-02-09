a5eda8500a73aecaa317bc671e363f4301b45434
Ryan Bucinell <ryan.bucinell@dayautomation.com>
2022-02-09 10:21:28 -0500

    version 1 solver

---
+++ wordle.js
@@ -0,0 +1,134 @@
+let list = [];
+
+//Check Answer
+let game     = document.querySelector("game-app").shadowRoot.querySelector("game-theme-manager > #game");
+let board    = game.querySelector("#board-container #board");
+let keyboard = game.querySelector('game-keyboard').shadowRoot.querySelector('#keyboard');
+
+function updateList()
+{
+    console.log( "updating list");
+    console.log(`list size: ${list.length}` );
+    let allBadLetters = [...new Set(regexGroups.map( g => g.bad ).flat())];
+    list.filter( word => {
+        let hasBadLetter = false;
+        allBadLetters.forEach( letter => {if( word.includes(letter)) hasBadLetter = true;})
+        return hasBadLetter;
+    })
+    console.log(`list size: ${list.length}` );
+}
+
+function evaluateTiles(tiles )
+{
+    tiles.forEach( (tile,index) => {		
+		console.log( `letter: ${tile.letter}, evaluation: ${tile.evaluation}, list.length=${list.length}` );
+        //updateRegexGroups( index, tile.letter, tile.evaluation);
+        if( tile.evaluation === 'correct' )
+		{
+			//remove any word that doesn't have the character at that location
+			list = list.filter( word => word.charAt(index) === tile.letter );
+		}
+		else if( tile.evaluation === 'absent')
+		{
+            let guess = tiles.map( t => t.letter).join('');
+			let letterCountInGuess = guess.split(tile.letter).length -1;
+			if( letterCountInGuess === 1)
+			{
+				//simple case where guess only contains 1 instance of letter, remove from list if a word contains that letter.
+				list = list.filter( word => !word.includes( tile.letter ));
+			}
+			else{
+				
+                list = list.filter( word => Object.entries(word.split('').reduce((prev, curr) => prev + curr === tile.letter)) < letterCountInGuess);
+			}
+        }
+		else if( tile.evaluation === 'present')
+		{
+            list = list.filter( word => word.includes(tile.letter) && word.charAt(index) !== tile.letter);
+		}
+	});
+
+}
+
+function getNextGuess()
+{
+    return guess_naive();
+}
+
+function guess_naive()
+{
+    //Pick a random word
+    let randomIndex = Math.floor(Math.random()*list.length);
+    guess = list[ randomIndex ];
+}
+
+async function resetList()
+{
+    list = [];
+    list = await (async ()=>  fetch('https://gist.githubusercontent.com/cfreshman/a03ef2cba789d8cf00c08f767e0fad7b/raw/5d752e5f0702da315298a6bb5a771586d6ff445c/wordle-answers-alphabetical.txt').then(r=>r.text()))();
+    list = list.split('\n');
+}
+
+async function wait( milliseconds )
+{
+    await new Promise(r => setTimeout(r, milliseconds ));
+}
+
+async function main()
+{
+    await resetList();
+    for( let guessCount = 1; guessCount <= 6; guessCount++ )
+    {   
+        //get the tiles for the current guess we are on
+        let row = board.querySelector(`game-row:nth-child(${guessCount})`).shadowRoot;
+        let tiles = [...row.querySelectorAll(".row game-tile")].map( tile => {
+            return {
+                'tile': tile,
+                letter: tile.getAttribute('letter'),
+                evaluation: tile.getAttribute('evaluation')
+        }});
+
+        let isNewGuess = false;
+        try{
+            isNewGuess = row.querySelector('.row game-tile').shadowRoot.querySelector('.tile').getAttribute('data-state') === 'empty';
+        }catch(e)
+        {
+            isNewGuess = false;
+        }        
+        let guess = '';
+        //determine if this is an existing guess or not
+        if( !isNewGuess )
+        {
+            guess = tiles.map( t => t.letter).join('');
+            console.log( `Existing guess found ${guess}`);
+        }
+        else
+        {
+            guess = getNextGuess();
+            console.log( `New guess is: ${guess} at index ${randomIndex}`);
+            //Type in Guess
+            guess.split('').forEach( l => keyboard.querySelector(`button[data-key="${l}"]`).click() );
+            //Hit Submit
+            keyboard.querySelector('button[data-key="↵"]').click(); //enter
+            //Animation on tiles
+            await wait( 2000 );
+        }
+
+        //Update tiles with evaluation
+        tiles = [...row.querySelectorAll(".row game-tile")].map( tile => {
+            return {
+                'tile': tile,
+                letter: tile.getAttribute('letter'),
+                evaluation: tile.getAttribute('evaluation')
+        }});
+
+        if( tiles.every( t => t.evaluation === 'correct') )
+        {
+            console.log( 'Solved the puzzle!');
+            break;
+        }
+        //Evaluate current guess
+        evaluateTiles(tiles);
+    }
+}
+main();
